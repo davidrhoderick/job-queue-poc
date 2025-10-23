fc1e83b439c9b08668a1ec72a0e206762949be27
David Rhoderick <david.e.rhoderick@gmail.com>
2025-10-23 16:15:09 +0200

    Moving worker to mutation

--- server/src/schema/operationQueue/resolvers/Mutation/testOperationStatus.ts
+++ server/src/schema/operationQueue/resolvers/Mutation/testOperationStatus.ts
@@ -1,5 +1,28 @@
 import { getJobState, queue } from "../Query/operationStatus";
 import type { MutationResolvers } from "../../../../generated/types.generated";
+import { Worker } from "bullmq";
+
+const testWorker = new Worker(
+	"test",
+	async (job) => {
+		const delayMs = job.data.duration * 1000;
+
+		await new Promise((resolve) => {
+			setTimeout(resolve, delayMs);
+		});
+
+		if (job.data.shouldFail)
+			throw new Error("Failing on purpose because shouldFail=true");
+
+		return {
+			anything: "we want to return",
+		};
+	},
+	{
+		connection: { host: "localhost", port: 6379 },
+	},
+);
+
 export const testOperationStatus: NonNullable<
 	MutationResolvers["testOperationStatus"]
 > = async (_parent, { input: { duration, shouldFail } }) => {
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,29 +2,7 @@ import { createYoga, createSchema } from "graphql-yoga";
 import { createServer } from "node:http";
 import { typeDefs } from "./generated/typeDefs.generated";
 import { resolvers } from "./generated/resolvers.generated";
-import { Worker } from "bullmq";
 
 const yoga = createYoga({ schema: createSchema({ typeDefs, resolvers }) });
 const server = createServer(yoga);
 server.listen(3000);
-
-const testWorker = new Worker(
-	"test",
-	async (job) => {
-		const delayMs = job.data.duration * 1000;
-
-		await new Promise((resolve) => {
-			setTimeout(resolve, delayMs);
-		});
-
-		if (job.data.shouldFail)
-			throw new Error("Failing on purpose because shouldFail=true");
-
-		return {
-			anything: "we want to return",
-		};
-	},
-	{
-		connection: { host: "localhost", port: 6379 },
-	},
-);
