235e7e8313172931de33243c8c427a5fbe17c5ae
davidrhoderick <david.e.rhoderick@gmail.com>
2025-10-24 14:09:27 +0200

    Adding codegen watching on client

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,6 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
 	schema: "server/src/generated/schema.generated.graphqls",
 	documents: ["app/**/*.tsx"],
-	ignoreNoDocuments: true,
 	generates: {
 		"./app/gql/": {
 			preset: "client",
@@ -11,6 +10,9 @@ const config: CodegenConfig = {
 				useTypeImports: true,
 			},
 		},
+	},
+	hooks: {
+		afterAllFileWrite: ["npm run format"],
 	},
 };
 
